a321cbcadef1a14eade61465f56c09dece6a2298
enzzotj <enzzojaco@gmail.com>
2021-08-10 16:33:13 -0300

    Finalizado

--- Scripts/HRoads_DML.sql
+++ Scripts/HRoads_DML.sql
@@ -10,9 +10,18 @@ VALUES ('Cruzado', NULL, NULL), ('Caçadora de Demônios', NULL, NULL),
 	   ('Necromante', NULL, NULL), ('Feiticeiro', NULL, NULL);
 GO
 
+UPDATE CLASSE
+SET nomeClasse = 'Necromancer'
+WHERE idClasse = 6;
+
 INSERT INTO PERSONAGEM (idClasse ,nomePersonagem, dataAtualizacao, dataCriacao)
 VALUES (1, 'DeuBug', 'Data Atual', '18/01/2019'), (2, 'BitBug', 'Data Atual', '17/03/2016'), (3, 'Fer8', 'Data Atual', '18/03/2018');
 GO
+
+UPDATE PERSONAGEM
+SET nomePersonagem = 'Fer7'
+WHERE idPersonagem = 3;
+
 
 INSERT INTO TIPO_HABILIDADE (tipoHabilidade)
 VALUES ('Ataque'), ('Defesa'), ('Cura'), ('Magia');
