513c45f367d65799bd8eb279458609973bcfb01e
enzzotj <enzzojaco@gmail.com>
2021-08-10 15:53:45 -0300

    DDL, DML e DQL

--- Scripts/HRoads_DML.sql
+++ Scripts/HRoads_DML.sql
@@ -1,22 +1,32 @@
 USE HROADS;
 GO
 
-INSERT INTO PERSONAGEM (nomePersonagem, dataAtualizacao, dataCriacao)
-VALUES ('DeuBug', 'Data Atual', '18/01/2019'), ('BitBug', 'Data Atual', '17/03/2016'), ('Fer8', 'Data Atual', '18/03/2018');
+INSERT INTO CLASSE (nomeClasse, capMaxVida, capMaxMana)
+VALUES ('Bárbaro', '100', '80'), ('Monge', '70', '100'), ('Arcanista', '75', '60');
 GO
 
-INSERT INTO CLASSE (idPersonagem, nomeClasse, capMaxVida, capMaxMana)
-VALUES (1, 'Bárbaro', '100', '80'), (2, 'Monge', '70', '100'), (3, 'Arcanista', '75', '60');
+INSERT INTO CLASSE (nomeClasse, capMaxVida, capMaxMana)
+VALUES ('Cruzado', NULL, NULL), ('Caçadora de Demônios', NULL, NULL), 
+	   ('Necromante', NULL, NULL), ('Feiticeiro', NULL, NULL);
 GO
 
-INSERT INTO HABILIDADE (nomeHabilidade)
-VALUES ('Lança Mortal'), ('Escudo Supremo'), ('Recuperar Vida');
+INSERT INTO PERSONAGEM (idClasse ,nomePersonagem, dataAtualizacao, dataCriacao)
+VALUES (1, 'DeuBug', 'Data Atual', '18/01/2019'), (2, 'BitBug', 'Data Atual', '17/03/2016'), (3, 'Fer8', 'Data Atual', '18/03/2018');
 GO
+
+INSERT INTO TIPO_HABILIDADE (tipoHabilidade)
+VALUES ('Ataque'), ('Defesa'), ('Cura'), ('Magia');
+GO
+
+INSERT INTO HABILIDADE (idTipoHabilidade ,nomeHabilidade)
+VALUES (1, 'Lança Mortal'), (2,'Escudo Supremo'), (3, 'Recuperar Vida');
+GO
+
 
 INSERT INTO CLASSE_HABILIDADE(idClasse, idHabilidade)
 VALUES (1, 1), (1,2), (2,2), (2,3), (3, NULL);
 GO
 
-INSERT INTO TIPO_HABILIDADE (idHabilidade, tipoHabilidade)
-VALUES (1, 'Ataque'), (2, 'Defesa'), (3, 'Cura'), (NULL, 'Magia');
+INSERT INTO CLASSE_HABILIDADE(idClasse, idHabilidade)
+VALUES (4, 2), (5,1), (7,3);
 GO
